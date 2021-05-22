18cf3bc56cac0ec4a4b2539def7392e6d6b2f944
mesut aygun <mesutaygun35@gmail.com>
2021-05-22 14:17:13 +0300

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Mesut Aygün on 22.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookWorm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BookWorm
+//
+//  Created by Mesut Aygün on 22.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
